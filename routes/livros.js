4951ad4f526a76f9cc84093bea61fe9d637012df
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2020-02-03 22:01:43 +0000

    adicionado route para livros

---
+++ routes/livros.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const livrosController = require('../app/api/controllers/livros');
+router.get('/', livrosController.page);
+router.get('/all', livrosController.getAll);
+router.post('/', livrosController.create);
+router.get('/livro', livrosController.detalhes);
+router.get('/:livroId', livrosController.getById);
+router.put('/:livroId', livrosController.updateById);
+router.delete('/:livroId', livrosController.deleteById);
+module.exports = router;
